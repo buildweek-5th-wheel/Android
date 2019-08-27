98b003de9521b0809ea03a8575cb6d036d049473
corgipower <greensborodogtraining@gmail.com>
2019-08-27 18:24:17 -0400

    intent for adapter, needs testing

--- buildweek-5th-wheel/app/src/main/java/com/example/buildweek_5th_wheel/utli/RVOwnerAdapter.kt
+++ buildweek-5th-wheel/app/src/main/java/com/example/buildweek_5th_wheel/utli/RVOwnerAdapter.kt
@@ -6,13 +6,21 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.buildweek_5th_wheel.R
+import com.example.buildweek_5th_wheel.activity.RVListingDetails
 import kotlinx.android.synthetic.main.rv_owners_list_item.view.*
 
 class RVOwnerAdapter() : RecyclerView.Adapter<RVOwnerAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.rv_owners_list_item, parent, false) as View)
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.rv_owners_list_item,
+                parent,
+                false
+            ) as View
+        )
     }
 
     override fun getItemCount(): Int {
@@ -20,7 +28,13 @@ class RVOwnerAdapter() : RecyclerView.Adapter<RVOwnerAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindModel()
+        holder.listImage.setImageResource(R.drawable.ic_launcher_background)
+        holder.listTitle.text = "Sample text"
+        holder.listDesc.text = "Ben"
+        holder.btn.setOnClickListener {
+            val intent = Intent(it.context, RVListingDetails::class.java)
+            startActivity(it.context, intent, null)
+        }
     }
 
 
@@ -28,11 +42,14 @@ class RVOwnerAdapter() : RecyclerView.Adapter<RVOwnerAdapter.ViewHolder>() {
         val listImage: ImageView = view.rv_owners_list_item_image
         val listTitle: TextView = view.rv_owners_list_item_title
         val listDesc: TextView = view.rv_owners_list_item_desc
+        val btn: Button = view.btn_details
 
         fun bindModel() {
             listImage.setImageResource(R.drawable.ic_launcher_background)
             listTitle.text = "Sample text"
-            listDesc.text = "Ben—not his real name, as he wishes to keep his name private a little longer—is a tall, 30-something"
+            listDesc.text =
+                "Ben—not his real name, as he wishes to keep his name private a little longer—is a tall, 30-something"
+
         }
     }
 }
